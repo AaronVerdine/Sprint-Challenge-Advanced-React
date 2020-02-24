8583ec030d934677e507abd1f6865adf3b07d2fe
Your name <a.m.verdine@gmail.com>
2020-02-23 21:07:19 -0500

    Update App.test.js with tests for PlayerCard component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+import PlayerCard from "./PlayerCard";
 
 describe("App", () => {
   it("renders without crashing", () => {
@@ -9,17 +10,35 @@ describe("App", () => {
   });
 
   it("should match 'ballers'", () => {
-    // const wrapper = rtl.render(<App />);
     const { getByText } = render(<App />);
 
-    // expect("Footballers").toMatch(/ballers/);
     getByText(/ballers/i);
   });
 
   it("array should contain 'Ertz'", () => {
-    // const wrapper = rtl.render(<App />);
     const { queryByText } = render(<App />);
 
     queryByText(/Ertz/i);
+  });
+});
+
+describe("PlayerCard", () => {
+  it("renders without crashing", () => {
+    render(<PlayerCard />);
+  });
+
+  it("should contain className player-list-grid-view", () => {
+    const { queryByText } = render(<PlayerCard />);
+    queryByText(/player-list-grid-view/i);
+  });
+
+  it("Card color is blue", () => {
+    const { findByText } = render(<PlayerCard />);
+    findByText(/blue/i);
+  });
+
+  it("Card Header contians className card-header", () => {
+    const { findByText } = render(<PlayerCard />);
+    findByText(/card-header/i);
   });
 });
