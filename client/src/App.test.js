1c9d74182add6fbd15440cd15d59ca2b0949e277
Your name <a.m.verdine@gmail.com>
2020-02-23 20:21:09 -0500

    Refactor App.test.js to include describe block

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,19 +1,25 @@
 import React from "react";
-import * as rtl from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const wrapper = rtl.render(<App />);
+describe("App", () => {
+  it("renders without crashing", () => {
+    render(<App />);
+  });
 
-  console.log(wrapper.debug());
-});
+  it("should match 'ballers'", () => {
+    // const wrapper = rtl.render(<App />);
+    const { getByText } = render(<App />);
 
-it("should match 'ballers'", () => {
-  const wrapper = rtl.render(<App />);
-  expect("Footballers").toMatch(/ballers/);
-});
+    // expect("Footballers").toMatch(/ballers/);
+    getByText(/ballers/i);
+  });
 
-it("array should contain 'Ertz'", () => {
-  const wrapper = rtl.render(<App />);
+  it("array should contain 'Ertz'", () => {
+    // const wrapper = rtl.render(<App />);
+    const { queryByText } = render(<App />);
+
+    queryByText(/Ertz/i);
+  });
 });
